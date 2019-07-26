410bdef1e3349a1e981fc1288fb509460da755ea
Josh Wilson <wiljo912@student.otago.ac.nz>
2019-07-26 14:06:42 +1200

    Adding a main menu

--- src/main/java/Administration.java
+++ src/main/java/Administration.java
@@ -1,3 +1,6 @@
+
+import gui.Mainmenu;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +13,9 @@
  */
 public class Administration {
     public static void main(String[] args) {
-        
+        Mainmenu frame = new Mainmenu();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
     }
 }
