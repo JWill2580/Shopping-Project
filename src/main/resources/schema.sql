6d170d36f896c10f77e00f40ba51ef8624402b1d
Josh Wilson <wiljo912@student.otago.ac.nz>
2019-08-09 13:40:13 +1200

    Adding product sql and adding dependency
    Reference #5.

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  wiljo912
+ * Created: 9/08/2019
+ */
+
+create table Product (
+    Product_ID integer,
+    Product_Name varchar(50) not null,
+    Description varchar(50) not null,
+    Category varchar(50) not null,
+    List_Price decimal(5, 2) not null,
+    Quantity_In_Stock decimal(5, 2) not null,
+    constraint Product_ID primary key (Product_ID)
+);
