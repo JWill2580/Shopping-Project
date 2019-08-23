2ede396764ccd3789a6db631b265fa88d9d871d3
admin <admin@admins-a-6.student.uod.otago.ac.nz>
2019-08-23 13:21:16 +1200

    Updating plantUML diagrams
    Reference #8.

--- src/main/java/Administration.java
+++ src/main/java/Administration.java
@@ -1,13 +1,13 @@
 
 import dao.DbManageProducts;
 import gui.Mainmenu;
-
+ 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+ 
 /**
  *
  * @author wiljo912
@@ -18,6 +18,6 @@ public class Administration {
         Mainmenu frame = new Mainmenu(dao);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-
+ 
     }
 }
